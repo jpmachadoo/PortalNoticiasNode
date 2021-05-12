9bc7653d9b04a3853f1b41d9a2842982fd09c541
jpmachadoo <jp_machado99@hotmail.com>
2021-05-11 21:45:42 -0300

    Adicionando as views e configurando as rotas

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ var app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.send('<html><body>Portal de notícias</body></html>');
+ 	res.render("home/index");
 });
 
-app.get('/tecnologia', (req, res) => {
-    res.send('<html><body>Notícias de Tecnologia</body></html>');
-  });
+app.get('/adicionar_noticia', (req, res) => {
+	res.render("admin/form_add_noticia");
+});
+
+app.get('/noticias', (req, res) => {
+	res.render("noticias/noticias");
+});
+
 
 app.listen(3000, console.log('Servidor rodando com express'));
